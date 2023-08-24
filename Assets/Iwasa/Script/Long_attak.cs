8d0c54377a1f55ecbd62f6176334b01bee2a1013
maturibayasik317 <sdt2395gp0002@edu.sca.ac.jp>
2023-08-24 17:44:55 +0900

    no message

--- Assets/Iwasa/Script/Long_attak.cs
+++ Assets/Iwasa/Script/Long_attak.cs
@@ -5,7 +5,6 @@ using UnityEditor;
 
 public class Long_attak : MonoBehaviour
 {
-    
     void Start()
     {
         StartCoroutine(SearchAndShot());
@@ -15,8 +14,13 @@ public class Long_attak : MonoBehaviour
     {
         while(true)
         {
-            //”ÍˆÍ“à‚É“G‚ª‚¢‚½‚ç‘Å‚Â
+            // ã“ã“ã§æ•µã‚’æ¢ã—ã¦æ’ƒã¤
             yield return new WaitForSeconds(1.0f);
+	    var collider = Physics2D.OverlapCircle(transform.position,2.0f,LayerMask.GetMask("Enemy"));
+        if(collider != null)
+        {
+            Destroy(collider.gameObject);
+        }
         }
     }
 }
